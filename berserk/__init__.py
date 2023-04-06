b1e1d2d20bc8da34202943934357a477bdb19e1a
Benedikt Werner <1benediktwerner@gmail.com>
2023-04-06 16:26:50 +0200

    Remove unnecessary noqa comments

--- berserk/__init__.py
+++ berserk/__init__.py
@@ -11,17 +11,17 @@ __email__ = berserk_metadata["Author-emai"]
 __version__ = berserk_metadata["Version"]
 
 
-from .clients import Client  # noqa: F401
-from .session import TokenSession  # noqa: F401
-from .session import Requestor  # noqa: F401
-from .enums import PerfType  # noqa: F401
-from .enums import Variant  # noqa: F401
-from .enums import Color  # noqa: F401
-from .enums import Room  # noqa: F401
-from .enums import Mode  # noqa: F401
-from .enums import Position  # noqa: F401
-from .enums import Reason  # noqa: F401
-from .formats import JSON  # noqa: F401
-from .formats import LIJSON  # noqa: F401
-from .formats import NDJSON  # noqa: F401
-from .formats import PGN  # noqa: F401
+from .clients import Client
+from .session import TokenSession
+from .session import Requestor
+from .enums import PerfType
+from .enums import Variant
+from .enums import Color
+from .enums import Room
+from .enums import Mode
+from .enums import Position
+from .enums import Reason
+from .formats import JSON
+from .formats import LIJSON
+from .formats import NDJSON
+from .formats import PGN
