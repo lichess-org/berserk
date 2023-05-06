fc7abc57dbafcdb02f6020897145981511562eba
Benedikt Werner <1benediktwerner@gmail.com>
2023-05-07 00:49:29 +0200

    Fix unused import pyright errors in __init__.py

--- berserk/__init__.py
+++ berserk/__init__.py
@@ -6,7 +6,7 @@ berserk_metadata = metadata.metadata(__package__)
 
 
 __author__ = berserk_metadata["Author"]
-__email__ = berserk_metadata["Author-emai"]
+__email__ = berserk_metadata["Author-email"]
 __version__ = berserk_metadata["Version"]
 
 
@@ -21,6 +21,27 @@ from .enums import Mode
 from .enums import Position
 from .enums import Reason
 from .formats import JSON
+from .formats import JSON_LIST
 from .formats import LIJSON
 from .formats import NDJSON
+from .formats import NDJSON_LIST
 from .formats import PGN
+
+__all__ = [
+    "Client",
+    "TokenSession",
+    "Requestor",
+    "PerfType",
+    "Variant",
+    "Color",
+    "Room",
+    "Mode",
+    "Position",
+    "Reason",
+    "JSON",
+    "JSON_LIST",
+    "LIJSON",
+    "NDJSON",
+    "NDJSON_LIST",
+    "PGN",
+]
