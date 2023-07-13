06759f1675649cfef7df2c733e9e647eb02a6d8f
zen1405 <shreyaskanjalkar@gmail.com>
2023-07-13 14:38:29 -0700

    Implemented 2 API endpoints for Teams
    Implemented the following endpoints:
    	1. get_join_request
    	2. accept_join_request
    
    Updated README.rst

--- berserk/clients/teams.py
+++ berserk/clients/teams.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import Iterator, Any, Dict
 
 from .. import models
-from ..formats import NDJSON
+from ..formats import NDJSON, JSON_LIST
 from .base import BaseClient
 
 
@@ -49,4 +49,26 @@ class Teams(BaseClient):
         :param user_id: ID of a team member
         """
         path = f"/team/{team_id}/kick/{user_id}"
+        self._r.post(path)
+
+    def get_join_requests(
+        self, team_id: str, declined: bool = False
+    ) -> list[Dict[str, Any]]:
+        """Get pending join requests of your team
+
+        :param team_id: ID of a team
+        :param declined: Get the declined join requests
+        :return: List of join requests
+        """
+        path = f"/api/team/{team_id}/requests"
+        params = {"declined": declined}
+        return self._r.get(path, params=params, fmt=JSON_LIST)
+
+    def accept_join_request(self, team_id: str, user_id: str) -> None:
+        """Accept someone's request to join the team
+
+        :param team_id: ID of a team
+        :param user_id: ID of a member requesting to join
+        """
+        path = f"/api/team/{team_id}/request/{user_id}/accept"
         self._r.post(path)
