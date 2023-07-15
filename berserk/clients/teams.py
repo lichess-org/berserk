39359c10ec73c323927e552cdae4407b2e24bc9e
Benedikt Werner <1benediktwerner@gmail.com>
2023-07-15 22:41:04 +0200

    Cleanup

--- berserk/clients/teams.py
+++ berserk/clients/teams.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Iterator, Any, Dict
+from typing import Iterator, Any, Dict, List
 
 from .. import models
 from ..formats import NDJSON, JSON_LIST
@@ -11,6 +11,7 @@ class Teams(BaseClient):
     def get_members(self, team_id: str) -> Iterator[Dict[str, Any]]:
         """Get members of a team.
 
+        :param team_id: ID of the team to get members from
         :return: users on the given team
         """
         path = f"/api/team/{team_id}/users"
@@ -23,9 +24,9 @@ class Teams(BaseClient):
     ) -> None:
         """Join a team.
 
-        :param team_id: ID of a team
-        :param message: Optional request message, if the team requires one
-        :param password: Optional password, if the team requires one.
+        :param team_id: ID of the team to join
+        :param message: optional request message, if the team requires one
+        :param password: optional password, if the team requires one
         """
         path = f"/team/{team_id}/join"
         payload = {
@@ -37,7 +38,7 @@ class Teams(BaseClient):
     def leave(self, team_id: str) -> None:
         """Leave a team.
 
-        :param team_id: ID of a team
+        :param team_id: ID of the team to leave
         """
         path = f"/team/{team_id}/quit"
         self._r.post(path)
@@ -45,30 +46,30 @@ class Teams(BaseClient):
     def kick_member(self, team_id: str, user_id: str) -> None:
         """Kick a member out of your team.
 
-        :param team_id: ID of a team
-        :param user_id: ID of a team member
+        :param team_id: ID of the team to kick from
+        :param user_id: ID of the user to kick from the team
         """
         path = f"/team/{team_id}/kick/{user_id}"
         self._r.post(path)
 
     def get_join_requests(
         self, team_id: str, declined: bool = False
-    ) -> list[Dict[str, Any]]:
+    ) -> List[Dict[str, Any]]:
         """Get pending join requests of your team
 
-        :param team_id: ID of a team
-        :param declined: Get the declined join requests
-        :return: List of join requests
+        :param team_id: ID of the team to request the join requests from
+        :param declined: whether to show declined requests instead of pending ones
+        :return: list of join requests
         """
         path = f"/api/team/{team_id}/requests"
         params = {"declined": declined}
         return self._r.get(path, params=params, fmt=JSON_LIST)
 
     def accept_join_request(self, team_id: str, user_id: str) -> None:
-        """Accept someone's request to join the team
+        """Accept someone's request to join one of your teams
 
-        :param team_id: ID of a team
-        :param user_id: ID of a member requesting to join
+        :param team_id: ID of the team to accept the request for
+        :param user_id: ID of the user requesting to join
         """
         path = f"/api/team/{team_id}/request/{user_id}/accept"
         self._r.post(path)
