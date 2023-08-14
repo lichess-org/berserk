3e1ac88e94a81e9d365b8913c868c517ab18b8d8
kraktus <kraktus@users.noreply.github.com>
2023-08-14 19:07:01 +0200

    test response for `teams.search` and `teams.get_popular`

--- tests/clients/test_teams.py
+++ tests/clients/test_teams.py
@@ -1,6 +1,6 @@
 import pytest
 
-from berserk import Client, Team
+from berserk import Client, Team, PaginatedTeams
 from typing import List
 from utils import validate, skip_if_older_3_dot_10
 
@@ -17,3 +17,15 @@ class TestLichessGames:
     def test_teams_of_player(self):
         res = Client().teams.teams_of_player("Lichess")
         validate(List[Team], res)
+
+    @skip_if_older_3_dot_10
+    @pytest.mark.vcr
+    def test_get_popular(self):
+        res = Client().teams.get_popular()
+        validate(PaginatedTeams, res)
+
+    @skip_if_older_3_dot_10
+    @pytest.mark.vcr
+    def test_search(self):
+        res = Client().teams.search("lichess")
+        validate(PaginatedTeams, res)
