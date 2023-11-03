0d71722866f8bf6885244ea6b63eabaa64c82257
Anupya Pamidimukkala <anupya@hotmail.ca>
2023-11-03 15:17:36 -0400

    oop small typo

--- tests/clients/test_analysis.py
+++ tests/clients/test_analysis.py
@@ -12,7 +12,7 @@ class TestAnalysis:
     @skip_if_older_3_dot_10
     @pytest.mark.vcr
     def test_get_cloud_evaluation(self):
-        res = Client().analysis.get_cloud_eval(
+        res = Client().analysis.get_cloud_evaluation(
             fen="rnbqkbnr/ppp1pppp/8/3pP3/8/8/PPPP1PPP/RNBQKBNR b KQkq - 0 2",
         )
         validate(PositionEvaluation, res)
