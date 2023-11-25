eaa5bb3a63d5decbee15a5965916b0eddf3c0731
saitejag <saitejag1502@gmail.com>
2023-11-25 23:48:58 +0530

    format correction

--- check-endpoints.py
+++ check-endpoints.py
@@ -34,7 +34,7 @@ for endpoint, data in spec["paths"].items():
     endpoint_without_slash = endpoint[1:]
 
     # Replace parameter placeholders with regular expression
-    endpoint_regex = r'/' + re.sub(r"{[^/]+?}", r"[^/]+?", endpoint_without_slash)
+    endpoint_regex = r"/" + re.sub(r"{[^/]+?}", r"[^/]+?", endpoint_without_slash)
 
     # Check if endpoint or a variation of it is present in file
     if not re.search(endpoint_regex, clients_content):
