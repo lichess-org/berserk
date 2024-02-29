d3f4293e1482c65fc89260dce54d1323493528c0
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2024-02-29 00:33:57 -0500

    Add lila github action

---
+++ integration/sample.py
@@ -0,0 +1,14 @@
+import berserk
+
+session = berserk.TokenSession('lip_bobby')
+client = berserk.Client(session, base_url="http://lila:9663")
+
+me = client.account.get()
+assert me['id'] == 'bobby'
+
+email = client.account.get_email()
+assert email == 'bobby@localhost'
+
+assert client.account.get_kid_mode() == False
+client.account.set_kid_mode(True)
+assert client.account.get_kid_mode() == True
