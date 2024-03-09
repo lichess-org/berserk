611688ad42712464a0a2e4153c87d17e3d12f5c2
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2024-03-09 00:09:34 -0500

    test upgrading bot account

--- integration/test_lila_account.py
+++ integration/test_lila_account.py
@@ -1,10 +1,12 @@
 import berserk
 import pytest
 
+
 @pytest.fixture(scope="module")
 def client():
     session = berserk.TokenSession('lip_bobby')
-    client = berserk.Client(session, base_url="http://lila:9663")
+    client = berserk.Client(
+        session, base_url="http://lila:9663")
     yield client
 
 
@@ -17,7 +19,22 @@ def test_account_get_email(client):
     assert client.account.get_email() == 'bobby@localhost'
 
 
-def test_update_kid_mode(client):
+def test_account_get_preferences(client):
+    preferences = client.account.get_preferences()
+    assert preferences['language'] == 'en-US'
+    assert preferences['prefs']['animation'] == 2
+
+
+def test_account_kid_mode(client):
     assert client.account.get_kid_mode() == False
     client.account.set_kid_mode(True)
     assert client.account.get_kid_mode() == True
+
+
+def test_account_upgrade_to_bot():
+    session = berserk.TokenSession('lip_zerogames')
+    client = berserk.Client(
+        session, base_url="http://lila:9663")
+    assert 'title' not in client.account.get()
+    client.account.upgrade_to_bot()
+    assert client.account.get()['title'] == "BOT"
