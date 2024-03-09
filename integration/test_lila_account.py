d72f900500f48acfce8d091269c5d2d8b198544f
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2024-03-08 22:42:15 -0500

    cleanup tests

--- integration/test_lila_account.py
+++ integration/test_lila_account.py
@@ -1,24 +1,23 @@
 import berserk
+import pytest
 
-def test_account_get():
+@pytest.fixture(scope="module")
+def client():
     session = berserk.TokenSession('lip_bobby')
     client = berserk.Client(session, base_url="http://lila:9663")
+    yield client
 
+
+def test_account_get(client):
     me = client.account.get()
     assert me['id'] == 'bobby'
 
 
-def test_account_get_email():
-    session = berserk.TokenSession('lip_bobby')
-    client = berserk.Client(session, base_url="http://lila:9663")
-
+def test_account_get_email(client):
     assert client.account.get_email() == 'bobby@localhost'
 
 
-def test_account_get_set_kid_mode():
-    session = berserk.TokenSession('lip_bobby')
-    client = berserk.Client(session, base_url="http://lila:9663")
-
+def test_update_kid_mode(client):
     assert client.account.get_kid_mode() == False
     client.account.set_kid_mode(True)
     assert client.account.get_kid_mode() == True
