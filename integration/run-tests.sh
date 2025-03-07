9d6c69ddc247a015f06649d599bb41001a0e1540
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2025-03-06 21:45:25 -0500

    update lila-docker port

--- integration/run-tests.sh
+++ integration/run-tests.sh
@@ -4,7 +4,7 @@ python3 -m pip install -e . --no-cache-dir
 
 attempts=0
 while [ $attempts -lt 30 ]; do
-    if curl -s http://bdit_lila:9663 >/dev/null; then
+    if curl -s http://bdit_lila:8080 >/dev/null; then
         break
     fi
     echo "⌛ Waiting for lila to start..."
