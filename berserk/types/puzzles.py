ed6261c1b6f94db30b3f71fd5a3fed4685ca1c70
Shoaib-Programmer <shoaibnigam422@gmail.com>
2025-04-21 12:26:55 +0000

    Added types

--- berserk/types/puzzles.py
+++ berserk/types/puzzles.py
@@ -1,6 +1,39 @@
 from __future__ import annotations
 
-from typing_extensions import TypedDict
+from typing_extensions import TypedDict, List
+
+from .common import LightUser, ClockConfig
+
+
+class PuzzleInfo(TypedDict):
+    """Structure containing information about a specific puzzle."""
+
+    id: str
+    rating: int
+    plays: int
+    solution: List[str]
+    themes: List[str]
+    initialFen: str
+    initialPly: int
+    gameId: str
+
+
+class PuzzleGame(TypedDict):
+    """Structure containing information about the game the puzzle originated from."""
+
+    id: str
+    clock: ClockConfig
+    perf: str
+    pgn: str
+    players: List[LightUser]
+    rated: bool
+
+
+class NextPuzzle(TypedDict):
+    """Structure representing the response for the next puzzle request."""
+
+    puzzle: PuzzleInfo
+    game: PuzzleGame
 
 
 class PuzzleRace(TypedDict):
