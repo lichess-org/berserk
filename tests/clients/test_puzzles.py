258b7d90765ae89f7aba5ec0e64935cb5bd44433
Charles B <charlesboudreault1@gmail.com>
2025-06-06 05:45:20 +0000

    renamed test class to TestPuzzles, to include clients name

--- tests/clients/test_puzzles.py
+++ tests/clients/test_puzzles.py
@@ -4,10 +4,10 @@ from berserk import Client, PuzzleData, PuzzleRace
 from utils import validate, skip_if_older_3_dot_10
 
 
-class TestLichessGames:
+class TestPuzzles:
     @skip_if_older_3_dot_10
     @pytest.mark.vcr
-    def test_get_puzzle_next(self):
+    def test_get_next(self):
         """Validate that the response matches the typed-dict"""
         res = Client().puzzles.get_next(angle="anastasiaMate", difficulty="hardest")
         validate(PuzzleData, res)
