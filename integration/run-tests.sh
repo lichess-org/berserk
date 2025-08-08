6972c6d62aa0d4b0a15affcf672b3ead3f1750f1
kraktus <kraktus@users.noreply.github.com>
2025-08-08 09:32:55 +0200

    finally fix integration tests locally

--- integration/run-tests.sh
+++ integration/run-tests.sh
@@ -3,11 +3,12 @@
 python3 -m pip install -e . --no-cache-dir
 
 attempts=0
+echo "🔍 Checking if lila is ready..."
 while [ $attempts -lt 30 ]; do
-    if curl -s http://bdit_lila:8080 >/dev/null; then
+    if [ "$(curl -s -o /dev/null -w '%{http_code}' http://bdit_lila:8080)" -eq 200 ]; then
         break
     fi
-    echo "⌛ Waiting for lila to start..."
+    echo "⌛ Waiting for lila to start... (attempt $((attempts + 1)))"
     sleep 1
     attempts=$((attempts + 1))
 done
