427d53af84751f82b05b34d47487ad9f3a13a9c0
José Agustín Moreno Larios <agustin90m@gmail.com>
2025-10-25 17:14:50 -0600

    Added  unit test and cassette

---
+++ tests/clients/test_account.py
@@ -0,0 +1,20 @@
+import pytest
+from datetime import datetime, timezone, timedelta
+import berserk
+from berserk.utils import to_millis
+from berserk.types.account import Timeline
+from utils import validate, skip_if_older_3_dot_10
+
+BASE_URL = "http://localhost:9663"
+
+
+class TestAccount:
+    @skip_if_older_3_dot_10
+    @pytest.mark.vcr
+    def test_get_timeline(self):
+        session = berserk.TokenSession("API_KEY")  # Add your own
+        client = berserk.Client(session, base_url=BASE_URL)
+        since = datetime.now(timezone.utc) - timedelta(days=14)
+        nb = 15
+        res = client.account.get_timeline(since=to_millis(since), nb=nb)
+        validate(Timeline, res)
