ba01cd6bd4c8c35d97f144735d5f2e857d1cb370
José Agustín Moreno Larios <agustin90m@gmail.com>
2025-11-30 11:31:02 -0600

    Comment change to check if existing integration tests run successfully

--- release.py
+++ release.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # Helper script to create and publish a new `berserk` release.
 # Based on `release.py` from `python-chess`.
+# THIS IS A COMMENT USED TO FORCE A PR FOR TESTING PURPOSES
 
 import argparse
 import os
