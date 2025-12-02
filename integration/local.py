95386fa4dc36f0004f924fb530d0c5adb3f62b0c
José Agustín Moreno-Larios <agustin90m@gmail.com>
2025-12-02 09:40:30 -0600

    Merge pull request #147 from JAMoreno-Larios/integration_test_script_fix
    Defaulting to running Python version if project .python-version is missing

--- integration/local.py
+++ integration/local.py
@@ -20,6 +20,7 @@ import logging.handlers
 import subprocess
 import sys
 import time
+import platform
 
 from watchdog.events import PatternMatchingEventHandler, FileSystemEvent
 from watchdog.observers import Observer
@@ -130,8 +131,8 @@ def get_project_python_version() -> str | None:
     try:
         with open(SCRIPT_DIR.parent / ".python-version", "r") as f:
             return f.read().strip()
-    except:
-        return None
+    except FileNotFoundError:
+        return platform.python_version()
 
 
 class ChangeHandler(PatternMatchingEventHandler):
